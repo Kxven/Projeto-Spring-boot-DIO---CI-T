74f57af2cfbbdb65af49e5c43219469da2035f25
Kxven <kevengodinho3474@gmail.com>
2025-11-24 16:51:51 -0300

    base do projeto

---
+++ src/main/java/dio/projeto_dio_spring/ProjetoDioSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.projeto_dio_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/projeto_dio_spring/ProjetoDioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.projeto_dio_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
