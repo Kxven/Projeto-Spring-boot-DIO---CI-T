707be5b6d27f8f32a1407d07a1dcb9acf19f47a1
Kxven <kevengodinho3474@gmail.com>
2025-11-25 20:54:00 -0300

    teste da aplicação

---
+++ src/main/java/dio/projeto_dio_spring/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package dio.projeto_dio_spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController{
+    @GetMapping
+    public String welcome(){
+        return "Welcome to my Spring Boot web API";
+    }
+}
