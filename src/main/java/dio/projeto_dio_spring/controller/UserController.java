24527d2dfb3462f7ce5cd0b9cc8aeb4f2d2171c9
Kxven <kevengodinho3474@gmail.com>
2025-11-25 21:42:24 -0300

    Rest Controller

---
+++ src/main/java/dio/projeto_dio_spring/controller/UserController.java
@@ -0,0 +1,32 @@
+package dio.projeto_dio_spring.controller;
+
+import dio.projeto_dio_spring.model.User;
+import dio.projeto_dio_spring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+public class UserController{
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return repository.listAll();
+     }
+     @GetMapping("/users/{username}")
+     public User getOne(@PathVariable("username") String username){
+        return repository.finByName(username);
+     }
+
+     @DeleteMapping("/users/{id}")
+     public void delete(@PathVariable("id") Integer id){
+        repository.remove(id);
+    }
+
+    @PostMapping("users")
+    public void postUser(@RequestBody User user){
+        repository.save(user);
+    }
+}
---
+++ src/main/java/dio/projeto_dio_spring/service/UserService.java
@@ -0,0 +1,4 @@
+package dio.projeto_dio_spring.service;
+
+public class UserService{
+}
